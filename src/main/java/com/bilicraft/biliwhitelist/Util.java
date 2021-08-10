11764d54257f12e9c297a1298db0a76143379c86
Ghost_chu <2908803755@qq.com>
2021-08-10 12:32:36 +0800

    broadcast the actions to everyone

--- src/main/java/com/bilicraft/biliwhitelist/Util.java
+++ src/main/java/com/bilicraft/biliwhitelist/Util.java
@@ -1,12 +1,13 @@
 package com.bilicraft.biliwhitelist;
 
-import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.ProxyServer;
 
 public class Util {
     public static void broadcastToAdmins(String content){
-        BiliWhiteList.instance.getProxy().getPlayers().stream().filter(p->p.hasPermission("biliwhitelist.admin")).forEach(p->p.sendMessage(content));
+        //BiliWhiteList.instance.getProxy().getPlayers().stream().filter(p->p.hasPermission("biliwhitelist.admin")).forEach(p->p.sendMessage(content));
+        ProxyServer.getInstance().broadcast(content);
     }
-    public static void broadcastToAdmins(BaseComponent content){
-        BiliWhiteList.instance.getProxy().getPlayers().stream().filter(p->p.hasPermission("biliwhitelist.admin")).forEach(p->p.sendMessage(content));
-    }
+//    public static void broadcastToAdmins(BaseComponent content){
+//        BiliWhiteList.instance.getProxy().getPlayers().stream().filter(p->p.hasPermission("biliwhitelist.admin")).forEach(p->p.sendMessage(content));
+//    }
 }
--- src/main/java/com/bilicraft/biliwhitelist/WhiteListCommand.java
+++ src/main/java/com/bilicraft/biliwhitelist/WhiteListCommand.java
@@ -60,16 +60,16 @@ public class WhiteListCommand extends Command {
                 BiliWhiteList.instance.getWhitelisted().add(uuid);
                 BiliWhiteList.instance.saveWhitelisted();
                 sender.sendMessages(ChatColor.GREEN + "添加成功：" + args[1] + " # " + uuid);
-                BiliWhiteList.instance.getLogger().info(ChatColor.GREEN + "白名单添加成功：" + args[1] + " # " + uuid+", 操作员："+sender.getName());
-                Util.broadcastToAdmins(ChatColor.GREEN + "白名单添加成功：" + args[1] + " # " + uuid+", 操作员："+sender.getName());
+                BiliWhiteList.instance.getLogger().info(ChatColor.GREEN + "白名单添加成功：" + args[1] + " # " + uuid + ", 操作员：" + sender.getName());
+                Util.broadcastToAdmins(ChatColor.GREEN + "白名单添加成功：" + args[1] + ", 操作员：" + sender.getName());
                 break;
             case "del":
             case "remove":
                 if (BiliWhiteList.instance.getWhitelisted().remove(uuid)) {
                     BiliWhiteList.instance.saveWhitelisted();
                     sender.sendMessages(ChatColor.GREEN + "白名单删除成功：" + args[1] + " # " + uuid);
-                    BiliWhiteList.instance.getLogger().info(ChatColor.GREEN + "白名单删除成功：" + args[1] + " # " + uuid+", 操作员："+sender.getName());
-                    Util.broadcastToAdmins(ChatColor.GREEN + "白名单删除成功：" + args[1] + " # " + uuid+", 操作员："+sender.getName());
+                    BiliWhiteList.instance.getLogger().info(ChatColor.GREEN + "白名单删除成功：" + args[1] + " # " + uuid + ", 操作员：" + sender.getName());
+                    Util.broadcastToAdmins(ChatColor.GREEN + "白名单删除成功：" + args[1] + ", 操作员：" + sender.getName());
                 } else {
                     sender.sendMessages(ChatColor.RED + "玩家不在白名单中：" + args[1] + " # " + uuid);
                 }
@@ -78,7 +78,7 @@ public class WhiteListCommand extends Command {
                 sender.sendMessage(ChatColor.GREEN + "白名单玩家：" + BiliWhiteList.instance.getWhitelisted());
                 break;
             case "query":
-                sender.sendMessage(ChatColor.GREEN+"该玩家白名单状态："+BiliWhiteList.instance.getWhitelisted().contains(uuid));
+                sender.sendMessage(ChatColor.GREEN + "该玩家白名单状态：" + BiliWhiteList.instance.getWhitelisted().contains(uuid));
                 break;
             default:
                 sender.sendMessage(ChatColor.RED + "参数有误");
